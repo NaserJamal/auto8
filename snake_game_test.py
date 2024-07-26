2ca69e97ead7dfcacbbaa4c7e1eabc86587caf0a
NaserJamal <naser.dll@hotmail.com>
2024-07-26 09:41:07 +0300

    added: e127_continuation_line_over_indented

--- snake_game_test.py
+++ snake_game_test.py
@@ -27,10 +27,8 @@ class SnakeGame:
 
     def generate_food(self):
         while True:
-            food = (random.randint(0, (self.width - self.cell_size)
-                                   // self.cell_size) * self.cell_size,
-                    random.randint(0, (self.height - self.cell_size) //
-                    self.cell_size) * self.cell_size)
+            food = (random.randint(0, (self.width - self.cell_size) // self.cell_size) * self.cell_size,
+                    random.randint(0, (self.height - self.cell_size) // self.cell_size) * self.cell_size)
             if food not in self.snake:
                 return food
 
